4d4a89532a89efd51fe409de48c7eafd5a00cda7
Lama <labbas@uwo.ca>
2022-10-26 17:35:12 -0400

    Blood cell conversions both ways

--- LoJam2022/Assets/Scripts/CellTypeManager.cs
+++ LoJam2022/Assets/Scripts/CellTypeManager.cs
@@ -14,19 +14,35 @@ public class CellTypeManager : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
+        
+    }
 
-        /*if (bloodType == BloodType.BloodCell)
-        {
+    public void BloodToCancer()
+    {
+        GetComponent<CellNavMesh>().enabled = false;
+        GetComponent<CancerNavMesh>().enabled = true;
 
-        }
-        else
-        {
+        tag = "Cancerous";
+        bloodType = BloodType.CancerousCell;
 
-        }*/
+        transform.Find("Blood").gameObject.SetActive(false);
+        transform.Find("Cancer").gameObject.SetActive(true);
+    }
+
+    public void CancerToBlood()
+    {
+        GetComponent<CellNavMesh>().enabled = true;
+        GetComponent<CancerNavMesh>().enabled = false;
+
+        tag = "Blood Cell";
+        bloodType = BloodType.BloodCell;
+
+        transform.Find("Blood").gameObject.SetActive(true);
+        transform.Find("Cancer").gameObject.SetActive(false);
     }
 }
